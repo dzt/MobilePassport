fb416c08066b807657ee73950e590170a62cc256
Ivan Chau <ivanmchau@gmail.com>
2016-01-18 14:22:33 -0500

    iOS reinit

---
+++ iOS/MobilePassport-iOS/MobilePassport-iOSTests/MobilePassport_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  MobilePassport_iOSTests.swift
+//  MobilePassport-iOSTests
+//
+//  Created by Ivan Chau on 1/18/16.
+//  Copyright © 2016 Ivan Chau & Peter Soboyejo. All rights reserved.
+//
+
+import XCTest
+@testable import MobilePassport_iOS
+
+class MobilePassport_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
