9e85f8a223802af84f1fb8d9864846450f085f4d
Ivan Chau <ivanmchau@gmail.com>
2016-01-20 16:33:58 -0500

    register done

--- ios/ios/RegisterViewController.swift
+++ ios/ios/RegisterViewController.swift
@@ -1,30 +1,26 @@
 //
-//  LoginViewController.swift
+//  RegisterViewController.swift
 //  ios
 //
-//  Created by Ivan Chau on 1/19/16.
+//  Created by Ivan Chau on 1/20/16.
 //  Copyright © 2016 Ivan Chau & Peter Soboyejo. All rights reserved.
 //
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class RegisterViewController: UIViewController {
     @IBOutlet weak var username : UITextField!
     @IBOutlet weak var password : UITextField!
-    @IBOutlet weak var login : UIButton!
+    @IBOutlet weak var email : UITextField!
+    @IBOutlet weak var name : UITextField!
     @IBOutlet weak var register : UIButton!
     
-    @IBAction func login(sender:UIButton){
-        if (self.username.text == "" || self.password.text == ""){
-            let alertView = UIAlertController(title: "UWOTM8", message: "Fam, what you tryna pull?", preferredStyle: .Alert)
-            let OK = UIAlertAction(title: "Is it 2 l8 2 say sorry", style: .Default, handler: nil)
-            alertView.addAction(OK)
-            self.presentViewController(alertView, animated: true, completion: nil);
-            return;
-        }
+    @IBAction func register(sender : UIButton){
+        print("register")
+        name.resignFirstResponder()
         username.resignFirstResponder()
         password.resignFirstResponder()
-        
+        email.resignFirstResponder()
         let headers = [
             "cache-control": "no-cache",
             "content-type": "application/x-www-form-urlencoded"
@@ -32,13 +28,17 @@ class LoginViewController: UIViewController {
         
         let usernameString = "username=" + self.username.text!
         let passwordString = "&password=" + self.password.text!
+        let emailString = "&email=" + self.email.text!
+        let nameString = "&string=" + self.name.text!
         let postData = NSMutableData(data: usernameString.dataUsingEncoding(NSUTF8StringEncoding)!)
         postData.appendData(passwordString.dataUsingEncoding(NSUTF8StringEncoding)!)
+        postData.appendData(emailString.dataUsingEncoding(NSUTF8StringEncoding)!)
+        postData.appendData(nameString.dataUsingEncoding(NSUTF8StringEncoding)!)
         
-        let request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:3000/login")!,
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:3000/signup")!,
             cachePolicy: .UseProtocolCachePolicy,
             timeoutInterval: 10.0)
-        request.HTTPMethod = "POST"
+        request.HTTPMethod = "PUT"
         request.allHTTPHeaderFields = headers
         request.HTTPBody = postData
         
@@ -52,7 +52,7 @@ class LoginViewController: UIViewController {
                 
                 if (httpResponse?.statusCode == 200){
                     dispatch_async(dispatch_get_main_queue(), {
-                        //segue to main view.
+                        //segue to main view, etc.
                     })
                 }else{
                     print("error")
@@ -64,28 +64,31 @@ class LoginViewController: UIViewController {
         
         dataTask.resume()
         
+
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.register.layer.cornerRadius = 0.5 * register.bounds.size.width
+        self.navigationItem.hidesBackButton = true;
         
         // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
+
     /*
     // MARK: - Navigation
-    
+
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
     */
-    
+
 }
