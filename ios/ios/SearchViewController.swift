923934301817d3a558a11830c28e7fb7608000d3
Ivan Chau <ivanmchau@gmail.com>
2016-01-22 22:41:06 -0500

    search

---
+++ ios/ios/SearchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchViewController.swift
+//  ios
+//
+//  Created by Ivan Chau on 1/22/16.
+//  Copyright © 2016 Ivan Chau & Peter Soboyejo. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
