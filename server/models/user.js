52ee7d98f783245fc34b7cddb4d936463e1a5623
Peter Soboyejo <thepcmrtim@gmail.com>
2016-01-22 21:35:55 -0500

    uhmmm

--- server/models/user.js
+++ server/models/user.js
@@ -7,10 +7,7 @@ var userSchema = mongoose.Schema({
     username: String,
     password: String,
     email: String,
-    raw_avatar_url_16: String,
-  	raw_avatar_url_32: String,
-  	raw_avatar_url_64: String,
-  	avatar_url_150: String
+  	avatar: String
 });
 
 userSchema.methods.generateHash = function(password) {
@@ -20,37 +17,5 @@ userSchema.methods.generateHash = function(password) {
 userSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
 };
-
-userSchema.virtual('avatar_url_16').get(function() {
-  if (this.use_gravatar) {
-    return "//gravatar.com/avatar/" + md5(this.email) + ".png?s=16";
-  } else {
-    return this.raw_avatar_url_16;
-  }
-});
-
-userSchema.virtual('avatar_url_32').get(function() {
-  if (this.use_gravatar) {
-    return "//gravatar.com/avatar/" + md5(this.email) + ".png?s=32";
-  } else {
-    return this.raw_avatar_url_32;
-  }
-});
-
-userSchema.virtual('avatar_url_64').get(function() {
-  if (this.use_gravatar) {
-    return "//gravatar.com/avatar/" + md5(this.email) + ".png?s=64";
-  } else {
-    return this.raw_avatar_url_64;
-  }
-});
-
-userSchema.virtual('avatar_url_150').get(function() {
-  if (this.use_gravatar) {
-    return "//gravatar.com/avatar/" + md5(this.email) + ".png?s=150";
-  } else {
-    return this.raw_avatar_url_64;
-  }
-});
 
 module.exports = mongoose.model('User', userSchema);
