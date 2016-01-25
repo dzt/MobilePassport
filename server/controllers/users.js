32e1dc460d826704dbdb08c6367885f181727811
Ivan Chau <ivanmchau@gmail.com>
2016-01-24 23:14:14 -0500

    lit

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -33,14 +33,18 @@ module.exports.login = function(req, res, next) {
         passport.authenticate('local', function(err, user, info) {
             if (err)
                 return next(err);
-            if (!user)
-                res.writeHead(400, {"Content-Type": "application/json"});
-                return res.json({ SERVER_RESPONSE: 0, SERVER_MESSAGE: "Wrong credentials" });
+            if(!user)
+                return res.json({SERVER_RESPONSE: 0, SERVER_MESSAGE: "Fucked Up"})
             req.logIn(user, function(err) {
                 if (err)
                     return next(err);
-                res.writeHead(200, {"Content-Type": "application/json"});
-                return res.json({ SERVER_RESPONSE: 1, SERVER_MESSAGE: "Logged in!" });
+                if (!err)
+                     /*if (!user)
+                        res.writeHead(400, {"Content-Type": "application/json"});
+                        return res.json({ SERVER_RESPONSE: 0, SERVER_MESSAGE: "Wrong credentials" });*/
+                   
+                    return res.json({ SERVER_RESPONSE: 1, SERVER_MESSAGE: "Logged in!" });
+                
             });
         })(req, res, next);
 };
